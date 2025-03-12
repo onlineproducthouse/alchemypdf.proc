b2418ebf04f52247c99966167a8066532991b620
Bongani Masuku <bongani@1702tech.com>
2025-03-12 19:33:15 +0200

    OPH-101 - change api port

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,6 +12,6 @@ export type Config = {
 export const _config = (): Config => ({
   protocol: process.env.HTMLTOPDF_PROTOCOL || "http",
   host: process.env.HTMLTOPDF_HOST || "localhost",
-  port: parseInt(process.env.HTMLTOPDF_PORT || "3000", 10),
+  port: parseInt(process.env.HTMLTOPDF_PORT || "7891", 10),
   apiKeyList: (process.env.HTMLTOPDF_KEYS || "").split(",").filter(key => !!key),
 })
