0607777dd7a01229adb2c0923ee7253116c29553
Bongani Masuku <bongani@1702tech.com>
2025-03-12 19:39:13 +0200

    OPH-101 - debugging stack overflow error

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,13 @@ const app: Express = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(RequestIdGenerator())
 
 app.get('/api/HealthCheck/Ping', async (_: any, res: any) => res.status(200).send({
   statusCode: 200,
   message: "Ok"
 }))
 
-app.use(RequestIdGenerator())
 app.use(ApiKeyValidator(__config))
 
 app.post('/api/v1/convert', async (req: any, res: any, next: any) => {
@@ -32,9 +32,12 @@ app.post('/api/v1/convert', async (req: any, res: any, next: any) => {
   }
 })
 
-app.use((error: Error, _req: any, res: any, _next: any) => res.status(500).send({
+app.use((error: Error, _req: any, res: any, _next: any) => {
+  console.log(error)
+  return res.status(500).send({
   statusCode: 500,
   message: error.message
-}))
+  })
+})
 
 app.listen(__config.port, () => console.log(`[server]: HTML 2 PDF Converter is running at: ${__config.protocol}://${__config.host}:${__config.port}`))
