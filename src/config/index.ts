d820368636bfb332a312cb8b5b2636cc2dc4022f
Bongani Masuku <bongani@1702tech.com>
2025-03-31 20:52:57 +0200

    OPH-117 - add configuration for oph api

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,11 @@ export type Config = {
   host: string
   port: number
   apiKeyList: string[]
+
+  ophApiProtocol: string
+  ophApiHost: string
+  ophApiPort: number
+  ophApiApiKey: string
 }
 
 export const _config = (): Config => ({
@@ -14,4 +19,9 @@ export const _config = (): Config => ({
   host: process.env.HTMLTOPDF_HOST || "localhost",
   port: parseInt(process.env.HTMLTOPDF_PORT || "7891", 10),
   apiKeyList: (process.env.HTMLTOPDF_KEYS || "").split(",").filter(key => !!key),
+
+  ophApiProtocol: process.env.API_PROTOCOL || "http",
+  ophApiHost: process.env.API_HOST || "localhost",
+  ophApiPort: parseInt(process.env.API_PORT || "7890", 10),
+  ophApiApiKey: (process.env.API_KEYS || "").split(",").filter(key => !!key)[0],
 })
