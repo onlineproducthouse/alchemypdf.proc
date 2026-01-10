d7df6efbe895c10d5e4652af20b15f7615dabaa2
Bongani Masuku <bongani@1702tech.com>
2026-01-10 12:47:06 +0200

    set up config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,22 +6,12 @@ export type Config = {
   protocol: string
   host: string
   port: number
-  apiKeyList: string[]
-
-  ophApiProtocol: string
-  ophApiHost: string
-  ophApiPort: number
-  ophApiApiKey: string
+  apiKey: string
 }
 
 export const _config = (): Config => ({
-  protocol: process.env.HTMLTOPDF_PROTOCOL || "http",
-  host: process.env.HTMLTOPDF_HOST || "localhost",
-  port: parseInt(process.env.HTMLTOPDF_PORT || "7891", 10),
-  apiKeyList: (process.env.HTMLTOPDF_KEYS || "").split(",").filter(key => !!key),
-
-  ophApiProtocol: process.env.API_PROTOCOL || "http",
-  ophApiHost: process.env.API_HOST || "localhost",
-  ophApiPort: parseInt(process.env.API_PORT || "7890", 10),
-  ophApiApiKey: (process.env.API_KEYS || "").split(",").filter(key => !!key)[0],
+  protocol: process.env.ALCHEMYPDFAPI_PROTOCOL || "http",
+  host: process.env.ALCHEMYPDFAPI_HOST || "localhost",
+  port: parseInt(process.env.ALCHEMYPDFAPI_PORT || "7891", 10),
+  apiKey: (process.env.ALCHEMYPDFAPI_KEYS || "").split(",").filter(key => !!key)[0],
 })
