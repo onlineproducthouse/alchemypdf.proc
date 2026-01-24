53c404ab5f8ce49e32bd097e330908d82d877fba
Bongani Masuku <bongani@1702tech.com>
2026-01-24 10:12:21 +0200

    simplify config

--- packages/config/src/config/index.ts
+++ packages/config/src/config/index.ts
@@ -7,20 +7,13 @@ export type Environment = {
   apiBasePath: string
 }
 
-type InitConfig = {
-  host: string
-  local: Environment
-}
-
 export interface IConfig {
   apiKey(): string
   apiHost(): string
   apiUrl(): string
 }
 
-export function config(_config: InitConfig): IConfig {
-  const env = _config["local"]
-
+export function config(env: Environment): IConfig {
   const apiKey = (): string => env.apiKey;
   const apiHost = (): string => `${env.apiProtocol}://${env.apiHost}:${env.apiPort}`;
   const apiUrl = (): string => `${apiHost()}${env.apiBasePath}`;
