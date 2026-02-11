baeb7e4c5a0ed4f7421466ef353a0ac2f308f0d5
Bongani Masuku <bongani@1702tech.com>
2026-02-11 18:55:10 +0200

    refactor override callback logic

--- packages/processor/src/index.ts
+++ packages/processor/src/index.ts
@@ -21,8 +21,8 @@ const runProcessor = async (ioc: IoC): Promise<void> => {
 
     console.log("[alchemypdf.proc]: executing callback")
 
-    const callbackUrl = process.env.ALCHEMYPDF_OVERRIDE_CALLBACK === "true"
-      ? payload.callbackUrl.replace(/(?:127.0.0.1|locahost)/, process.env.ALCHEMYPDF_OVERRIDE_CALLBACK_VALUE || "api")
+    const callbackUrl = ioc.config.overrideCallback()
+      ? ioc.config.callbackUrl()
       : payload.callbackUrl
 
     const _callback = await ioc.alcheMyPdfAPI.callback(callbackUrl, {
